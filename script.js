404c78dad6c784e675545c9c802653064fd5ea85
Omar <omar.ssenhaji@gmail.com>
2025-11-14 13:16:18 +0100

    Add CSS style and event listener to button
    Added an event listener that displays the books
    form once the Add Book button is clicked
    Added more CSS styling to the buttons

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello')
+let bookLibrary = [];
+
+const addBookBtn = document.getElementById('add-book-btn');
+addBookBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    const bookForm = document.querySelector('.book-form');
+    bookForm.style.display = 'block';
+});
