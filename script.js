f9987a03dc9ff2c03aad17aee65df9f10e754da1
Omar <omar.ssenhaji@gmail.com>
2025-11-14 14:30:19 +0100

    Add Book constructor + function to add a new Book to the library
    Added a book constructor
    Added a function that gets the values from the
    form, and adds a new book with those values
    in the library array when the 'add book' button is
    clicked

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 let bookLibrary = [];
 
 const addBookBtn = document.getElementById('add-book-btn');
-addBookBtn.addEventListener('click', function(e){
-    e.preventDefault();
+addBookBtn.addEventListener('click', function(){
     const bookForm = document.querySelector('.book-form');
     bookForm.style.display = 'block';
 });
+
+const addBook = document.getElementById('submit-button');
+addBook.addEventListener('click', function(e) {
+    e.preventDefault();
+    addBookToLibrary();
+    console.log(bookLibrary);
+});
+
+
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function addBookToLibrary() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+
+    const newBook = new Book(title, author, pages, read);
+        bookLibrary.push(newBook);
+
+};
+
+
+
+
+
